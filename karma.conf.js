769fed989e83709a3fe72bde3205a0bd65cb4262
Nico Rehwaldt <git_nikku@nixis.de>
2018-03-22 22:49:06 +0100

    test(project): allow to specify test targets

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,22 @@
+// configures browsers to run test against
+// any of [ 'ChromeHeadless', 'Chrome', 'Firefox', 'IE', 'PhantomJS' ]
+var browsers =
+  (process.env.TEST_BROWSERS || 'PhantomJS')
+    .replace(/^\s+|\s+$/, '')
+    .split(/\s*,\s*/g)
+    .map(function(browser) {
+      if (browser === 'ChromeHeadless') {
+        process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+        // workaround https://github.com/GoogleChrome/puppeteer/issues/290
+        if (process.platform === 'linux') {
+          return 'ChromeHeadless_Linux';
+        }
+      } else {
+        return browser;
+      }
+    });
+
 module.exports = function(karma) {
   karma.set({
 
@@ -13,7 +32,7 @@ module.exports = function(karma) {
 
     reporters: [ 'progress' ],
 
-    browsers: [ 'PhantomJS' ],
+    browsers: [ browsers ],
 
     singleRun: true,
     autoWatch: false,
