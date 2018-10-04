0cdd7139c73f4e7f6e53479c3790c9cddc55b96b
Nico Rehwaldt <git_nikku@nixis.de>
2018-10-04 16:15:36 +0200

    test(prependTo): verify first child behavior

--- test/spec/prependTo.js
+++ test/spec/prependTo.js
@@ -20,4 +20,21 @@ describe('prependTo', function() {
     expect(result.parentNode).to.eql(svg);
   });
 
+
+  it('should prepend as first child', function() {
+
+    // given
+    var svg = create('<svg></svg>');
+    var group = create('<g id="G" />');
+
+    // when
+    var result = prependTo(group, svg);
+
+    // then
+    expect(result).to.equal(group);
+    expect(result.parentNode).to.eql(svg);
+
+    expect(svg.childNodes).to.have.length(1);
+  });
+
 });
