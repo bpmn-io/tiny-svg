a4a77c7ae2173ff951af46157b29545ac53a575b
Nico Rehwaldt <git_nikku@nixis.de>
2018-10-04 15:23:43 +0200

    feat(create): create any svg element via markup
    Allow non <SVG> elements to be created from markup:
    
    ```javascript
    var g = create('<g id="G" />');
    ```

--- test/spec/util/parse.js
+++ test/spec/util/parse.js
@@ -4,6 +4,7 @@ import {
 
 import parse from '../../../lib/util/parse';
 
+
 describe('parse', function() {
 
   it('should parse <defs> / CDATA', function() {
@@ -16,13 +17,26 @@ describe('parse', function() {
         ']]></style>' +
       '</defs>';
 
-
     // when
-    var doc = parse(text);
-    var svg = innerSVG(doc.documentElement);
+    var fragment = parse(text);
+    var svg = innerSVG(fragment);
 
     // then
     expect(svg).to.eql(text);
+  });
+
+
+  it('should parse <svg>', function() {
+
+    // given
+    var text = '<svg><g id="1"/><circle/></svg>';
+
+    // when
+    var doc = parse(text);
+    var svg = innerSVG(doc);
+
+    // then
+    expect(svg).to.eql('<svg xmlns="http://www.w3.org/2000/svg"><g id="1"/><circle/></svg>');
   });
 
 });
