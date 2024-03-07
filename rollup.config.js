0da58875c0f7cfd8822143e0cdca0fb0ca5bca24
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-03-07 07:54:03 +0100

    chore: drop pre-built UMD distribution
    BREAKING CHANGE: Use ESM distribution in favor of UMD

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,6 @@
-import { terser } from 'rollup-plugin-terser';
-
 import pkg from './package.json';
 
-const umdDist = 'dist/tiny-svg.js';
-
 export default [
-
-  // browser-friendly UMD build
-  {
-    input: 'lib/index.js',
-    output: {
-      name: 'TinySVG',
-      file: umdDist,
-      format: 'umd'
-    }
-  },
-  {
-    input: 'lib/index.js',
-    output: {
-      name: 'TinySVG',
-      file: umdDist.replace(/\.js$/, '.min.js'),
-      format: 'umd'
-    },
-    plugins: [
-      terser()
-    ]
-  },
   {
     input: 'lib/index.js',
     output: [
