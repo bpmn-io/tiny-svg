db73f506d46ed4a70a9968251b9487e055a70e00
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-05-29 21:05:20 +0200

    feat: use optimized `clear`
    Use `Element.replaceChildren` over looping through all SVG elements.
    
    This saves us some hops and re-renders potentially.
    
    Ref: https://developer.mozilla.org/en-US/docs/Web/API/Element/replaceChildren

---
+++ test/spec/clear.js
@@ -0,0 +1,66 @@
+import {
+  create,
+  clear,
+  select
+} from 'tiny-svg';
+
+
+describe('clear', function() {
+
+  it('should clear <svg />', function() {
+
+    // given
+    var svg = create(`
+      <svg>
+        <defs>
+          <circle id="myCircle" cx="0" cy="0" r="5" />
+
+          <linearGradient id="myGradient" gradientTransform="rotate(90)">
+            <stop offset="20%" stop-color="gold" />
+            <stop offset="90%" stop-color="red" />
+          </linearGradient>
+        </defs>
+
+        <use x="5" y="5" href="#myCircle" fill="url('#myGradient')" />
+
+        <rect x="10" y="10" width="20" height="20"></rect>
+        <rect x="20" y="20" width="20" height="20"></rect>
+        <rect x="30" y="30" width="20" height="20"></rect>
+        <rect x="40" y="40" width="20" height="20"></rect>
+      </svg>
+    `);
+
+    // when
+    var result = clear(svg);
+
+    // then
+    expect(result).to.equal(svg);
+    expect(result.childNodes.length).to.eql(0);
+  });
+
+
+  it('should clear <g />', function() {
+
+    // given
+    var svg = create(`
+      <svg>
+        <g>
+          <rect x="10" y="10" width="20" height="20"></rect>
+          <rect x="20" y="20" width="20" height="20"></rect>
+          <rect x="30" y="30" width="20" height="20"></rect>
+          <rect x="40" y="40" width="20" height="20"></rect>
+        </g>
+      </svg>
+    `);
+
+    var g = select(svg, 'g');
+
+    // when
+    var result = clear(g);
+
+    // then
+    expect(result).to.equal(g);
+    expect(result.childNodes.length).to.eql(0);
+  });
+
+});
